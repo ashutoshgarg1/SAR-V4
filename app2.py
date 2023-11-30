9d2588e5b196fcbf64446a23f8769aaf2515f449
prince aggarwal <prince183378@exlservice.com>
2023-11-30 20:29:20 +0530

    ne commit

--- app2.py
+++ app2.py
@@ -2391,10 +2391,10 @@ elif selected_option_case_type == "Money Laundering":
 
                                 query = "What are the associated suspicious transactions for Credit Card?"
                                 context_1 = docsearch2.similarity_search(query, k=5)
-                                prompt_1=f''' Your goal is to identify all the suspicious transactions from Credit_Card_statement. Suspicious transactions can be:\n\n
-                                Transactions that are made to a suspicious entity. Output "Description", "Date" and "Debited ($)" of those identified transactions. # Strictly do not repeat any transaction.\n\
+                                prompt_1=f''' Your goal is to identify the suspicious transactions from Credit_Card_statement. Suspicious transactions can be:\n\n
+                                Transactions made to a suspicious entity. Output "Description", "Date" and "Debited ($)" of those identified transactions. # Strictly do not repeat any transaction.\n\
                                 Context: {context_1}\n\
-                                Response: (Do not give/add any extra note, explanation in answer.) '''
+                                Response: (Do not give/add any extra Note, Explanation in answer.) '''
                                 st.write(context_1)
                                 system_prompt = wrap_prompt("You are a Money Laundering Analyst.", "system")
                                 user_prompt = wrap_prompt(prompt_1, "user")
@@ -2436,7 +2436,7 @@ elif selected_option_case_type == "Money Laundering":
                                 prompt_1=f''' Your goal is to identify the suspicious transactions from savings_account_statement. Suspicious transactions can be:\n\n
                                 High Value Cash Deposits in a short span of time. Strictly do not include any Paycheck transactions and Opening balance transaction as they may not be considered as suspicious transactions. Output the "Description", "Date" and "Credited ($)" of those identified transactions.Also, do not repeat the same transaction.\n\
                                 Context: {context_1}\n\
-                                Response: (Strictly do not give/add any note, explanation in answer.) '''
+                                Response: (Strictly do not give/add any Note, Explanation in answer.) '''
                                 #st.write(context_1)
                                 system_prompt = wrap_prompt("You are a Money Laundering Analyst.", "system")
                                 user_prompt = wrap_prompt(prompt_1, "user")

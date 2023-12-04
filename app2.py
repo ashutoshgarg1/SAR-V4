4dc5a6c88ce200bf2694480e693b093f8dd41706
prince aggarwal <prince183378@exlservice.com>
2023-12-04 14:20:28 +0530

    ne commit

--- app2.py
+++ app2.py
@@ -1203,7 +1203,7 @@ elif selected_option_case_type == "Fraud transaction dispute":
                                 contexts = docsearch.similarity_search(query, k=9)
                                 prompt = f" You are professional Fraud Analyst. Find answer to the questions as truthfully and in as detailed as possible as per given context only,\n\n\
                                     1. Check if The transaction/disputed amount > 5,000 USD value threshold, If Yes, then check below points to make sure if it is a suspicious activity or not: \n\
-                                    2. {response_1} analyse this response,if details matches or not? If matches then there is no suspicion else, it can be a suspicipos activity. (Also mention the mismatched details).\n\n\
+                                    2. {response_1} analyse this response,if details matches or not? If matches then there is no suspicion else, it can be a suspicious activity. (Concisely mention only the mismatched details).\n\n\
                                     3. If a potential suspect name is identified or not? Suspect is a person who has commited the fraud, If identified then this can be a suspicious activity, else not.\n\n\
                                     Even if transaction/disputed amount > 5,000 USD but if above criteria does not met, then this can not be considered as a suspicious activity. \n\n\
                                     Based on above points, give your recommendation if this is a case of suspicious activity or not? \n\n\

42bb491792703c8e9bd73b19c96fac47fafbe892
prince aggarwal <prince183378@exlservice.com>
2023-12-07 22:21:48 +0530

    New commit

--- OST.py
+++ OST.py
@@ -1882,7 +1882,7 @@ elif selected_option_case_type == "Fraud transaction dispute":
                 
                 def summ_llama_():
                     template = f"""Provide a detailed summary of the below Context and make sure to include all the relevant information (like names, transactions, involved parties, amounts involved, etc). Provide the summary in a single paragraph and don't include words like these: 'chat summary', 'includes information' or 'AI' in my final summary.
-                    {text1}
+                    ```{text1}```
                     Response: (Return your response in a single paragraph.) """
                     prompt = PromptTemplate(template=template,input_variables=["text1"])
                     llm_chain_llama = LLMChain(prompt=prompt,llm=llama_13b)

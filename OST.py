31bec7ad5556c42529966ec18f63783c5bffdb11
prince aggarwal <prince183378@exlservice.com>
2023-12-07 21:35:10 +0530

    New commit

--- OST.py
+++ OST.py
@@ -1942,7 +1942,7 @@ elif selected_option_case_type == "Fraud transaction dispute":
                             #     text.append(value)
                             #####
                             #st.write(text)
-                                
+                        
                             prompt_1 =  f'''Provide a DSetailed summary of the below Context and make sure to include all the relevant information (like names, transactions, involved parties, amounts involved, etc). Do not include details like customer id , case id etc. Provide the summary in a single paragraph and don't include words like these: 'chat summary', 'includes information' or 'AI' in my final summary.\n\n\
                             Context: {text}'''
                             response = llama_llm(llama_13b,prompt_1)
@@ -1950,7 +1950,8 @@ elif selected_option_case_type == "Fraud transaction dispute":
                             ####
                             # result = llm_chain_llama.run(text)
                             st.session_state["tmp_summary_llama"] = response
-                            #st.write("summaryyyy")
+                            st.write(text)
+                            st.write("summaryyyy")
                             st.write(response)
                             
                             #st.write(st.session_state["tmp_summary_llama"])

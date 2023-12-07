ccb21960cdba04ad44f9f69defb863d0103c98c8
prince aggarwal <prince183378@exlservice.com>
2023-12-07 15:57:36 +0530

    ne commit

--- OST.py
+++ OST.py
@@ -1470,9 +1470,9 @@ elif selected_option_case_type == "Fraud transaction dispute":
                                 query ="Give your recommendation if this is a Suspicious activity or not?"
                                 contexts = docsearch.similarity_search(query, k=5)
                                 prompt = f"You are professional Fraud Analyst. Find answer to the questions as truthfully and in as detailed as possible as per given context only,\n\n\
-                                    1. Check what is the transaction/disputed amount and if The transaction/disputed amount > 5,000 USD value threshold,If Yes, then check below points to make sure if it is a suspicious activity or not: \n\
+                                    1. Check what is the transaction/disputed amount and if The transaction/disputed amount > 5,000 USD value threshold. \n\
                                     2. {analyse} analyse this response,if invoice is billed to cardholder then there is no suspicion else, it can be a suspicious activity.\n\n\
-                                    3. who is supect idenfied ? If a suspect is identified from above ,then this can be considered as a suspicious activity else not.\n\n\
+                                    3. who is supect idenfied ? If a suspect is identified from above ,then this can be considered as a suspicious activity.\n\n\
                                     Even if transaction/disputed amount > 5,000 USD but if above criteria does not met, then this can not be considered as a suspicious activity. \n\n\
                                     Analyse above points properly and give your recommendation if this is a case of suspicious activity or not? \n\n\
                                     Context: {contexts}\n\

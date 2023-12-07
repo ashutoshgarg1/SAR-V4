59d57ba8c10aee76005bec69b71fa9528b865acb
prince aggarwal <prince183378@exlservice.com>
2023-12-07 21:33:49 +0530

    New commit

--- OST.py
+++ OST.py
@@ -1943,7 +1943,7 @@ elif selected_option_case_type == "Fraud transaction dispute":
                             #####
                             #st.write(text)
                                 
-                            prompt_1 =  f'''Act as a summarization tool and Provide a detailed summary of the below Context and make sure to include all the relevant information (like names, transactions, involved parties, amounts involved, etc). Do not include details like customer id , case id etc. Provide the summary in a single paragraph and don't include words like these: 'chat summary', 'includes information' or 'AI' in my final summary.\n\n\
+                            prompt_1 =  f'''Provide a detailed summary of the below Context and make sure to include all the relevant information (like names, transactions, involved parties, amounts involved, etc). Do not include details like customer id , case id etc. Provide the summary in a single paragraph and don't include words like these: 'chat summary', 'includes information' or 'AI' in my final summary.\n\n\
                             Context: {text}'''
                             response = llama_llm(llama_13b,prompt_1)
 

5d3ff1d96fd7f8a2d5fa519202136349b592e4d7
prince aggarwal <prince183378@exlservice.com>
2023-12-08 17:17:27 +0530

    New commit

--- app2.py
+++ app2.py
@@ -2702,7 +2702,7 @@ elif selected_option_case_type == "Money Laundering":
                             
                                 
                                 #Display table
-                                st.table(res_df_gpt)
+                                st.write(res_df_gpt)
 
                                 #copy in session state
                                 st.session_state["tmp_table_gpt_aml"] = pd.concat([st.session_state.tmp_table_gpt_aml, res_df_gpt], ignore_index=True)

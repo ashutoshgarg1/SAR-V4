1c1b97412ef6ed4a37aade87e01ba8aa921696f0
prince aggarwal <prince183378@exlservice.com>
2023-12-11 14:34:48 +0530

    ne commit

--- v4_test.py
+++ v4_test.py
@@ -2583,7 +2583,7 @@ elif selected_option_case_type == "Money Laundering":
 
                                 query = "What are the associated suspicious transactions for Credit Card?"
                                 context_1 = docsearch2.similarity_search(query, k=5)
-                                prompt_1=f''' Your goal is to identify the suspicious transactions only from Credit_Card_statement within the given context. Suspicious transactions can be:\n\n
+                                prompt_1=f''' Your goal is to identify the suspicious transactions only from Credit_Card_statement within the given Data. Suspicious transactions can be:\n\n
                                 Transactions made to a suspicious entity. Output "Description", "Date" and "Debited ($)" of each identified transactions as a numbered list of this format : "Description:  Date:  Debited ($):" . # Strictly do not repeat any transaction.\n\
                                 Context: {context_1}\n\
                                 Response: (Do not give/add any extra Note, Explanation in answer.) '''

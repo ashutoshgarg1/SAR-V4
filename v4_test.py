76f93b0e7391ca7c130784a41725c571e2baa936
prince aggarwal <prince183378@exlservice.com>
2023-12-12 22:51:36 +0530

    ne commit

--- v4_test.py
+++ v4_test.py
@@ -2920,32 +2920,32 @@ elif selected_option_case_type == "Money Laundering":
                                 
                             
 
-                                # template = """ Your goal is to identify the suspicious transactions from Checking_account_statement.\n\
-                                # Suspicious transactions can be:\n\
-                                # High Value Cash Deposits in a short span of time. Strictly do not include any Paycheck transactions and Opening balance transaction as they may not be considered as suspicious transactions. \n\\
-                                # Output the "Description", "Date" and "Credited ($)" of those identified transactions as a indexed list of this format : "Description:  Date:  Credited ($):" ."""
-                                # query = "What are the associated suspicious transactions for Checking account?"
+                                template = """ Your goal is to identify the suspicious transactions from Checking_account_statement.\n\
+                                Suspicious transactions can be:\n\
+                                High Value Cash Deposits in a short span of time. Strictly do not include any Paycheck transactions and Opening balance transaction as they may not be considered as suspicious transactions. \n\\
+                                Output the "Description", "Date" and "Credited ($)" of those identified transactions as a number list of this format : "Description:  Date:  Credited ($):" ."""
+                                query = "What are the associated suspicious transactions for Checking account?"
                             
-                                # response,context = run_chain_llm(template,query)
-                                # chat_history_1[query] = response
-                                # st.session_state["lineage_aml_llama"] = context
+                                response,context = run_chain_llm(template,query)
+                                chat_history_1[query] = response
+                                st.session_state["lineage_aml_llama"] = context
 
                                 ###
 
-                                query = "What are the associated suspicious transactions for Checking account?"
-                                context_1 = docsearch2.similarity_search(query, k=5)
+                                # query = "What are the associated suspicious transactions for Checking account?"
+                                # context_1 = docsearch2.similarity_search(query, k=5)
                                   
 
-                                prompt_1=f'''Your goal is to identify the suspicious transactions from Checking_account_statement.\n\
-                                Suspicious transactions can be:\n\
-                                High Value Cash Deposits in a short span of time. Strictly do not include any Paycheck transactions and Opening balance transaction as they may not be considered as suspicious transactions. \n\\
-                                Output the "Description", "Date" and "Credited ($)" of those identified transactions as a indexed list of this format : "Description:  Date:  Credited ($):". Also, do not repeat the same transaction.\n\
-                                Context: {context_1}\n\
-                                Response: (Strictly do not give/add any Note, Explanation in answer.) '''
-                                response = zephyr_llm(zephyr_7b,prompt_1)
+                                # prompt_1=f'''Your goal is to identify the suspicious transactions from Checking_account_statement.\n\
+                                # Suspicious transactions can be:\n\
+                                # High Value Cash Deposits in a short span of time. Strictly do not include any Paycheck transactions and Opening balance transaction as they may not be considered as suspicious transactions. \n\\
+                                # Output the "Description", "Date" and "Credited ($)" of those identified transactions as a indexed list of this format : "Description:  Date:  Credited ($):". Also, do not repeat the same transaction.\n\
+                                # Context: {context_1}\n\
+                                # Response: (Strictly do not give/add any Note, Explanation in answer.) '''
+                                # response = zephyr_llm(zephyr_7b,prompt_1)
 
-                                chat_history_1[query] = response
-                                st.session_state["lineage_aml_llama"] = context
+                                # chat_history_1[query] = response
+                                # st.session_state["lineage_aml_llama"] = context
                                 
                                 
                                 transactions_sa = response

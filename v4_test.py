b7e6dfd0b66f742c15074b616168fe6f095e2ec1
prince aggarwal <prince183378@exlservice.com>
2023-12-13 13:02:27 +0530

    ne commit

--- v4_test.py
+++ v4_test.py
@@ -2907,7 +2907,7 @@ elif selected_option_case_type == "Money Laundering":
                                 query = "What is the total amount associated with the money laundering activity for Credit card?"
                                 context_1 = amounts
                                 st.write(context_1)
-                                prompt_1 = f'''You are a calculator and your goal is add all the numbers in the below context as total calculated amount.\n\
+                                prompt_1 = f'''You are a calculator and your goal is add up all the numbers in the below context as "total calculated amount".\n\
                                 Context: {context_1}\n\
                                 Response: (Add this before the total calculated amount : "Total Money Laundering amount that can be associated with credit card is : ".)'''
 

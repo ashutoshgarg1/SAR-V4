0e488a584b749cfb1baabf88dea7eba995f9874a
prince aggarwal <prince183378@exlservice.com>
2023-12-14 12:56:10 +0530

    ne commit

--- v4_test.py
+++ v4_test.py
@@ -2896,8 +2896,8 @@ elif selected_option_case_type == "Money Laundering":
 
                                 query = "What is the total amount associated with the money laundering activity for Credit card?"
                                 context_1 = transactions_cc
-                                prompt_1 = f'''Act as a addition calculator and add up all the amounts listed in the context. Produce Output in one sentence only.\n\
-                                Output the total calculated amount as answer to the question. Only generate response. Do not generate anys reason.\n\
+                                prompt_1 = f'''Act as a calculator and add up all the debited amounts listed in the context. Only produce one sentence in the output.\n\
+                                Output the total calculated amount as answer to the question. Only generate response. Do not generate reason.\n\
                                 Context: {context_1}\n\
                                 Question: {query}\n\
                                 Response: '''

aedadffdeb3ce64d1b9ba46e0a65a81f0145549a
prince aggarwal <prince183378@exlservice.com>
2023-12-15 15:53:34 +0530

    ne commit

--- v4_test.py
+++ v4_test.py
@@ -1832,7 +1832,7 @@ elif selected_option_case_type == "Fraud transaction dispute":
                                    
             with col4_up:
                 def summ_gpt_(tmp_table_gpt):
-                    template = '''Provide a detailed summary of the below Context and make sure to include all the relevant information (like names, transactions, involved parties, amounts involved, etc). Do not include details like customer id , case id etc. Provide the summary in a single paragraph and don't include words like these: 'chat summary', 'includes information' or 'AI' in my final summary.\n\n\
+                    template = '''Provide a detailed summary of the below Context and make sure to include all the relevant information (like names, transactions, involved parties, amounts involved, etc). Do not include details like customer id , case id etc. Don't include words like these: 'chat summary', 'includes information' or 'AI' in my final summary.\n\n\
                             Context: {text}  '''
                     
                     prompt = PromptTemplate(template=template,input_variables=["text"])
@@ -1909,7 +1909,7 @@ elif selected_option_case_type == "Fraud transaction dispute":
                             #st.write(text)
                             prompt = f'''Provide an accurate and detailed summary of the below context and make sure to include all the important information (like names, transactions, involved parties, amounts involved, etc). Do not include details like customer id , case id etc. Don't include words like these: 'chat summary', 'includes information' or 'AI' in my final summary.\n\n\
                             context: {text1} \n\
-                            Response: (Provide the detailed summary in one paragraph.)'''
+                            Response: '''
                             response1 = zephyr_llm(zephyr_7b,prompt) 
                             
 

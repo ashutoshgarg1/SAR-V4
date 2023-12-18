c0b49cef8c97a07f7d5629aa26cfef6213916e97
prince aggarwal <prince183378@exlservice.com>
2023-12-18 19:34:10 +0530

    ne commit

--- v4_test.py
+++ v4_test.py
@@ -2220,6 +2220,7 @@ elif selected_option_case_type == "Fraud transaction dispute":
                 col_d1, col_d2 = st.tabs(["Download Report", "Download Case Package"])
 
                 with col_d1:
+                    st.write(doc)
                 # Applying to download button -> download_button
                     st.markdown("""
                         <style>
